84d92362c79780987f5a27152273d0c793aff976
vikoo <vikoo.vikoo90@gmail.com>
2026-03-06 10:44:34 +0530

    - logging

--- cron/data_upload/f1/f1_data_upload.py
+++ cron/data_upload/f1/f1_data_upload.py
@@ -12,9 +12,9 @@ from cron.utils import f1_graphql_token
 
 
 def process():
-    logger.info("######################")
-    logger.info("fetching the F1 schedule")
-    logger.info("######################")
+    logger.error("############################################")
+    logger.error("fetching the F1 schedule")
+    logger.error("############################################")
     logger.info(f"Using Token 1: {f1_graphql_token[:5]}*****")
     json_data = get_latest_past_race(is_f1_feed=True)
 
